5d83216d9385ffebcfd011845074fd1bd6141453
Suraj-chawhan <surajchauhan442918@gmail.com>
2025-02-17 17:50:50 +0530

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -117,8 +117,8 @@ const MainContent = () => {
             <Image
               src={card.image}
               alt={card.title}
-              width={50}
-              height={50}
+              width={300}
+              height={300}
               className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
             />
           </div>
