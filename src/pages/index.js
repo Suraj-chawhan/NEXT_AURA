5df7f37d332420b344ad35d594ec6fb28544d146
Suraj-chawhan <surajchauhan442918@gmail.com>
2025-02-17 17:49:01 +0530

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -115,11 +115,11 @@ const MainContent = () => {
         >
           <div className="relative aspect-square overflow-hidden">
             <Image
-              src="/Images/logo.jpg"
-              width={50} // Adjust as needed
-              height={50} // Adjust as needed
-              className="w-12 h-12 rounded-full border-2 border-white"
-              alt="Logo"
+              src={card.image}
+              alt={card.title}
+              width={50}
+              height={50}
+              className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
             />
           </div>
           <div className="p-6">
